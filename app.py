b5f86089308e50e6518f4eac0694f7525caefb41
Brian-15 <bcirieco@gmail.com>
2021-08-09 15:22:52 -0700

    added jinja templates for form and story pages

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, request
+import stories
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def base_page():
+    return render_template("form.html", words=stories.story.prompts)
+
+@app.route("/story")
+def generate_story():
+    answers = {
+        "verb": request.args["verb"],
+        "noun": request.args["noun"],
+        "place": request.args["place"],
+        "adjective": request.args["adjective"],
+        "plural_noun": request.args["plural_noun"]
+    }
     
+    return render_template("story.html", story_body = stories.story.generate(answers))
